01c8397ef1fbd6e3b8adf44f3d53191fee806ad4
jerry791 <65881812+jerry791@users.noreply.github.com>
2021-12-30 01:55:52 +0800

    2121

--- js/index.js
+++ js/index.js
@@ -63,7 +63,7 @@ function hot_spot(ary) {
         }
     }
     for (let i = 0; i < 6; i++) {
-        console.log(ary[ranary[i]].ScenicSpotID);
+        console.log(ary[ranary[i]].ScenicSpotName);
         SpotCard[i].setAttribute('onclick', `Send('` + ary[ranary[i]].ScenicSpotID + `',0)`);
         HotSpotImg[i].src = ary[ranary[i]].Picture.PictureUrl1;
         HotSpot[i].innerHTML = `<p>` + ary[ranary[i]].Address.substr(0, 3) + `<br><span>` + ary[ranary[i]].ScenicSpotName + `</span></p>`;
